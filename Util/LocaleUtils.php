1f61140f03655641aebd07eec511491a67fe2002
c1tru55 <kislicynda@gmail.com>
2015-07-29 20:54:53 +0600

    update locale utils

--- Util/LocaleUtils.php
+++ Util/LocaleUtils.php
@@ -8,6 +8,11 @@ namespace ITE\Common\Util;
 class LocaleUtils
 {
     /**
+     * @var \NumberFormatter
+     */
+    protected static $numberFormatter;
+    
+    /**
      * @param null $precision
      * @return int
      */
@@ -17,8 +22,50 @@ class LocaleUtils
             return $precision;
         }
 
-        $formatter = new \NumberFormatter(\Locale::getDefault(), \NumberFormatter::DECIMAL);
+        $formatter = self::getNumberFormatter();
 
-        return $formatter->getAttribute(\NumberFormatter::MAX_FRACTION_DIGITS);
+        return $formatter->getAttribute(\NumberFormatter::MAX_FRACTION_DIGITS); // FRACTION_DIGITS ?
+    }
+
+    /**
+     * @return int
+     */
+    public static function getGroupingSize()
+    {
+        $formatter = self::getNumberFormatter();
+
+        return $formatter->getAttribute(\NumberFormatter::GROUPING_SIZE);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getGroupingSeparatorSymbol()
+    {
+        $formatter = self::getNumberFormatter();
+
+        return $formatter->getSymbol(\NumberFormatter::GROUPING_SEPARATOR_SYMBOL);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getDecimalSeparatorSymbol()
+    {
+        $formatter = self::getNumberFormatter();
+
+        return $formatter->getSymbol(\NumberFormatter::DECIMAL_SEPARATOR_SYMBOL);
+    }
+
+    /**
+     * @return \NumberFormatter
+     */
+    protected static function getNumberFormatter()
+    {
+        if (null === self::$numberFormatter) {
+            self::$numberFormatter = new \NumberFormatter(\Locale::getDefault(), \NumberFormatter::DECIMAL);
+        }
+
+        return self::$numberFormatter;
     }
 } 

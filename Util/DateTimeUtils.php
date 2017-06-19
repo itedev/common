cd8c3bcb1d1c2141fa581652498602cc30c6b22c
c1tru55 <mr.c1tru55@gmail.com>
2017-06-19 21:07:38 +0600

    update datetime utils

--- Util/DateTimeUtils.php
+++ Util/DateTimeUtils.php
@@ -14,12 +14,41 @@ class DateTimeUtils
      * @param \DateTime $b
      * @return int
      */
-    public function compare(\DateTime $a, \DateTime $b)
+    public static function compare(\DateTime $a, \DateTime $b)
     {
         if ($a == $b) {
             return 0;
         }
 
         return $a < $b ? -1 : 1;
+    }
+
+    /**
+     * @param \DateTime|null $date
+     * @param \DateTime|null $time
+     * @return \DateTime|null
+     */
+    public static function createFromDateAndTime(\DateTime $date = null, \DateTime $time = null)
+    {
+        if (null === $date || null === $time) {
+            return null;
+        }
+
+        $dateTime = clone $date;
+        $dateTime->setTimezone($time->getTimezone());
+        $dateTime->setTime(
+            (int) $time->format('H'),
+            (int) $time->format('i'),
+            (int) $time->format('s')
+        );
+
+        //$dateTime = clone $time;
+        //$dateTime->setDate(
+        //    (int) $time->format('Y'),
+        //    (int) $time->format('n'),
+        //    (int) $time->format('j')
+        //);
+
+        return $dateTime;
     }
 }
